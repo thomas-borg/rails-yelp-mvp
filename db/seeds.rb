889c5e0f95a699138cea531529e2eb753788e8a7
Thomas Borg <thomaslborg7@gmail.com>
2024-05-22 17:39:41 +0200

    rails-yelp-mvp complete with minor styling on index, review form on restaurant show page not complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Restaurant.delete_all
+
+5.times {
+  restaurant = Restaurant.new(name: Faker::Restaurant.name, phone_number: Faker::PhoneNumber.phone_number, category: ["chinese", "italian", "japanese", "french", "belgian"].sample, address: Faker::Address.full_address)
+  restaurant.save
+
+  3.times {
+    review = Review.new(content: Faker::Restaurant.review, rating: rand(0..5))
+    review.restaurant = restaurant
+    review.save
+  }
+}
